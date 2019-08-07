94bb6de497e8cd438189f4e64da0c623ee78e11b
Wes Jonke <wesjonke@gmail.com>
2019-08-07 15:21:35 -0400

    initial project set up, custom middle ware set up, route for Get / set up

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code away!
+const express = require('express');
+const userRouter = require("./users/userRouter")
+
+const server = express();
+function logger(req, res, next) {
+  console.log(req.url, req.method, new Date().toISOString())
+  next()
+  };
+
+server.get('/', logger, (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`)
+});
+
+//custom middleware
+server.use(express.json())
+
+//routes
+server.use("/user", userRouter)
+
+
+
+server.listen(6666, () => {
+  console.log("Server UP WOOOOO! Listening on port 6666")
+})
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,17 +1,60 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
+const db =require( "./userDb")
 
-router.post('/', (req, res) => {
 
+///////////////////custom middleware///////////////////////
+
+const validateUserId = (req, res, next) => {
+  const id = req.params.id 
+  id ? next() : res.send({message: "invalid user"})
+}
+
+const validateUser = (req, res, next) => {
+  console.log(req.body)
+  if(!req.body || req.body === {}){
+    res.status(400).send({message: "missing data"})
+  }
+  if(!req.body.name){
+    res.status(400).send({message: "missing name field"})
+  }
+  next()
+}
+
+const validatePost = (req, res, next) => {
+  if(!req.body){
+    res.status(400).send({message: "missing data"})
+  }
+  if(!req.body.text){
+    res.status(400).send({message: "missing text field"})
+  }
+  next()
+}
+
+
+/////////////////////Routes///////////////////////////
+
+router.post('/', validateUser, (req, res) => {
+const info = req.body;
+db.insert(info).then(cb => res.send(cb)).catch(err => console.error(err))
 });
 
-router.post('/:id/posts', (req, res) => {
+// router.post('/:id/posts', (req, res) => {
+//   const id = req.params.id;
+//   let user;
+//   let posty = req.body.post
+  
+//   db.getById(id).then(cb => user = cb; console.log(user)).catch(err => console.error(err)).then(() =>{
+//     db.update(id, {...user, posts:[...user.post,{posty}]}).then(cb => res.send(cb)).catch(err => console.error(err))
 
-});
+//   })
+
+  
+// });
 
 router.get('/', (req, res) => {
-
+db.get("users").then(cb => res.send(cb)).catch(err=> console.error(err))
 });
 
 router.get('/:id', (req, res) => {
@@ -30,18 +73,8 @@ router.put('/:id', (req, res) => {
 
 });
 
-//custom middleware
 
-function validateUserId(req, res, next) {
 
-};
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
 
 module.exports = router;
