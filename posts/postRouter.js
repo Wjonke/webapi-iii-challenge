3102588ae1844a6ccfdc91e15ee2e9befa75a871
Wes Jonke <wesjonke@gmail.com>
2019-08-08 11:58:04 -0400

    cleaned up code

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,6 +3,37 @@ const express = require('express')
 const router = express.Router();
 const PostsDb = require("../posts/postDb");
 
+
+///////////custom middleware////////////
+
+function validatePostId(req, res, next) {
+  const ID = req.params.id;
+  PostsDb.getById(ID)
+    .then(post => {
+      if (post) {
+        req.post = post;
+        next();
+      } else {
+        res.status(400).json({ message: "Invalid Post ID" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Error validating post ID" });
+    });
+}
+
+const validatePost = (req, res, next) => {
+  if(!req.body){
+    res.status(400).send({message: "missing data"})
+  }
+  if(!req.body.text){
+    res.status(400).send({message: "missing text field"})
+  }
+  next()
+}
+
+//////////////////ROUTES//////////////
+
 //GET all posts
 router.get("/", (req, res) => {
   PostsDb.get()
@@ -13,6 +44,8 @@ router.get("/", (req, res) => {
       res.status(500).json({ message: "Could not get all posts" });
     });
 });
+
+
 
 //GET post by its ID
 router.get("/:id", validatePostId, (req, res) => {
@@ -29,7 +62,6 @@ router.get("/:id", validatePostId, (req, res) => {
 
 
 //create a user's post//
-
 router.post("/:id/posts", validatePost, (req, res) => {
   const postInfo = req.body;
   postInfo.user_id = req.params.id;
@@ -66,6 +98,8 @@ router.delete("/:id", validatePostId, (req, res) => {
     });
 });
 
+
+
 //UPDATE post using its ID and supplying changes in body
 router.put("/:id", validatePostId, validatePost, (req, res) => {
   const postId = req.params.id;
@@ -79,32 +113,6 @@ router.put("/:id", validatePostId, validatePost, (req, res) => {
     });
 });
 
-// custom middleware
 
-function validatePostId(req, res, next) {
-  const ID = req.params.id;
-  PostsDb.getById(ID)
-    .then(post => {
-      if (post) {
-        req.post = post;
-        next();
-      } else {
-        res.status(400).json({ message: "Invalid Post ID" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: "Error validating post ID" });
-    });
-}
-
-const validatePost = (req, res, next) => {
-  if(!req.body){
-    res.status(400).send({message: "missing data"})
-  }
-  if(!req.body.text){
-    res.status(400).send({message: "missing text field"})
-  }
-  next()
-}
 
 module.exports = router;
